b8a57f58e72e49f83063b545639c0d89c3a85c61
MrP01 <peter@waldert.at>
2023-07-11 17:36:08 +0100

    Solver computes both operators and plots solution

--- solver/solver.jl
+++ solver/solver.jl
@@ -1,16 +1,18 @@
 using ClassicalOrthogonalPolynomials, ContinuumArrays, Plots, Formatting, HypergeometricFunctions, SpecialFunctions
 import ContinuumArrays: MappedWeightedBasisLayout, Map, WeightedBasisLayout
+gaston()
 
-d = 1;  # dimension
+d = 1  # dimension
 m = 2  # integer
-alpha = 2.0
-beta = 8.0
+alpha = 2.2
+beta = 1.6
+R = 8  # radius of the interval [-R, R]
 @assert -d < alpha < 2 + 2 * m - d
 @assert beta > -d
 @assert m >= 0 && m == floor(m)
 
-function theorem216(r, n)
-  @show r, n
+function theorem216(r, n, beta)
+  @show r, n, beta
   prefactor =
     pi^(d / 2) *
     gamma(1 + beta / 2) *
@@ -46,24 +48,45 @@ ContinuumArrays.invmap(::InvQuadraticMap{T}) where {T} = QuadraticMap{T}()
 Base.getindex(d::QuadraticMap, x::Inclusion) = d
 Base.getindex(d::InvQuadraticMap, x::Inclusion) = d
 
+map = QuadraticMap();
+imap = InvQuadraticMap();
+
 # represent the basis P_n^(a,b)(2r^2-1)
-P = Jacobi(m - (alpha + d) / 2, (d - 2) / 2)[QuadraticMap(), :]
+B = Jacobi(m - (alpha + d) / 2, (d - 2) / 2);
+P = B[QuadraticMap(), :];
+x = axes(B, 1)
 r = axes(P, 1)
 
-N = 4  # order of basis expansion
-BigMatrix = zeros(N, N);
-for column in 1:N
-  func = theorem216(r, column)
-  f_N = P[:, 1:N] \ func
-  BigMatrix[:, column] .= f_N
+@assert x.domain == (-1 .. 1)  # Chebyshev
+@assert r.domain == (0 .. 1)  # Radial
+
+function solve(N)
+  AttractiveMatrix = zeros(N, N)
+  RepulsiveMatrix = zeros(N, N)
+  for column in 1:N
+    attractiveFunction = theorem216(r, column, alpha)
+    repulsiveFunction = theorem216(r, column, beta)
+    attractiveExpansionCoeffs = P[:, 1:N] \ attractiveFunction
+    repulsiveExpansionCoeffs = P[:, 1:N] \ repulsiveFunction
+    AttractiveMatrix[:, column] .= attractiveExpansionCoeffs
+    RepulsiveMatrix[:, column] .= repulsiveExpansionCoeffs
+  end
+
+  BigMatrix = (R^(alpha + d) / alpha) * AttractiveMatrix - (R^(beta + d) / beta) * RepulsiveMatrix
+  BigRHS = zeros(N)
+  BigRHS[1] = 1
+
+  BigSolution = BigMatrix \ BigRHS
+  return BigSolution
 end
 
-BigRHS = zeros(N);
-BigRHS[1] = 1;
+r_vec = 0:0.01:1
+x_vec = -1:0.01:1
 
-@show BigMatrix
-BigSolution = BigMatrix \ BigRHS;
+# y_vec_radial = vec(sum(BigSolution .* P[r_vec, 1:N]', dims=1));
+plot(x_vec, vec(sum(solve(2) .* P[abs.(x_vec), 1:2]', dims=1)), label="N = 2");
+plot!(x_vec, vec(sum(solve(3) .* P[abs.(x_vec), 1:3]', dims=1)), label="N = 3");
+plot!(x_vec, vec(sum(solve(4) .* P[abs.(x_vec), 1:4]', dims=1)), label="N = 4");
+plot!(x_vec, vec(sum(solve(5) .* P[abs.(x_vec), 1:5]', dims=1)), label="N = 5");
 
-x = 0:0.01:1
-y = vec(sum(BigSolution .* P[x, 1:N]', dims=1));
-plot(x, y)
+# spy(BigMatrix, ms=20, markershape=:rect)
