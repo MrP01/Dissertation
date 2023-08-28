368da857f2e8e16be0089559dc25a44ae6e6c7e8
MrP01 <peter@waldert.at>
2023-08-28 11:44:00 +0100

    correct spelling, add to acknowledgements

--- solver/analyticsolutions.jl
+++ solver/analyticsolutions.jl
@@ -6,7 +6,7 @@ function explicitSolution(x::AbstractVector{Float64}; p::Params.Parameters)
   if isa(p.potential, Params.AttractiveRepulsive)
     alpha, beta = p.potential.alpha, p.potential.beta
     if isapprox(alpha, 2.0; atol=1e-3) && -1.0 < beta < 2.0
-      # Solution from Carillo, 2017-explicit-solutions
+      # Solution from Carrillo, 2017-explicit-solutions
       prefactor = cos((2 - beta) * pi / 2) / ((beta - 1) * pi)
       R = (prefactor * SpecialFunctions.beta(0.5, (3 - beta) / 2))^(1 / (beta - 2))
       x *= R  # so this function accepts input from -1 to 1
